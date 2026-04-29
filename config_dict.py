7527424c039f02e032e5aec2d3d91a046a02ca6c
minh_ueb <131586636+minhueb7321@users.noreply.github.com>
2026-04-29 18:58:46 +0700

    Update config_dict.py

--- config_dict.py
+++ config_dict.py
@@ -1,3 +1,3 @@
 CONFIG_HN_DETAIL = {
     'Bán Buôn HN': [
-        {"start_date": "2026-01-03", "end_date": "2026-02-03", "ky_cong_no": "2026-01"}, # vi dụ vậy
+        {"start_date": "2026-01-03", "end_date": "2026-02-03", "ky_cong_no": "2026-01"}, # vi dụ
